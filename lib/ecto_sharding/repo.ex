f340a71bcb5f2e5119da5715c3ddd78b216ffd8b
Craig Day <cday@zendesk.com>
2017-09-26 17:53:00 -0700

    capture and pass super method

--- lib/ecto_sharding/repo.ex
+++ lib/ecto_sharding/repo.ex
@@ -2,19 +2,19 @@ defmodule Ecto.Sharding.Repo do
   defmodule QueryProcessing do
     alias Ecto.Sharding.ShardRegistry
 
-    defmacro process_queryable(method, args) do
+    defmacro process_queryable(method, super_call, args) do
       quote do
         %Ecto.Query{from: {_, model}} = List.first(unquote(args))
 
         if model.sharded? do
           apply(ShardRegistry.current_repo, unquote(method), unquote(args))
         else
-          super(unquote_splicing(args))
+          unquote(super_call).(unquote_splicing(args))
         end
       end
     end
 
-    defmacro process_schema(method, args) do
+    defmacro process_schema(method, super_call, args) do
       quote do
         struct = List.first(unquote(args))
         model = struct.__struct__
@@ -22,88 +22,88 @@ defmodule Ecto.Sharding.Repo do
         if model.sharded? do
           apply(ShardRegistry.current_repo, unquote(method), unquote(args))
         else
-          super(unquote_splicing(args))
+          unquote(super_call).(unquote_splicing(args))
         end
       end
     end
   end
 
   defmacro __using__(opts) do
-    quote do
-      import QueryProcessing, only: [process_queryable: 2, process_schema: 2]
-      use Ecto.Repo, unquote(opts)
+    quote bind_quoted: [opts: opts] do
+      import QueryProcessing, only: [process_queryable: 3, process_schema: 3]
+      use Ecto.Repo, opts
       alias Ecto.Sharding.ShardRegistry
 
       defoverridable Ecto.Repo
 
       def all(queryable, opts \\ []),
-        do: process_queryable(:all, [queryable, opts])
+        do: process_queryable(:all, &super(&1, &2), [queryable, opts])
 
       def stream(queryable, opts \\ []),
-        do: process_queryable(:stream, [queryable, opts])
+        do: process_queryable(:stream, &super(&1, &2), [queryable, opts])
 
       def get(queryable, id, opts \\ []),
-        do: process_queryable(:get, [queryable, id, opts])
+        do: process_queryable(:get, &super(&1, &2, &3), [queryable, id, opts])
 
       def get!(queryable, id, opts \\ []),
-        do: process_queryable(:get!, [queryable, id, opts])
+        do: process_queryable(:get!, &super(&1, &2, &3), [queryable, id, opts])
 
       def get_by(queryable, clauses, opts \\ []),
-        do: process_queryable(:get_by, [queryable, clauses, opts])
+        do: process_queryable(:get_by, &super(&1, &2, &3), [queryable, clauses, opts])
 
       def get_by!(queryable, clauses, opts \\ []),
-        do: process_queryable(:get_by!, [queryable, clauses, opts])
+        do: process_queryable(:get_by!, &super(&1, &2, &3), [queryable, clauses, opts])
 
       def one(queryable, opts \\ []),
-        do: process_queryable(:one, [queryable, opts])
+        do: process_queryable(:one, &super(&1, &2), [queryable, opts])
 
       def one!(queryable, opts \\ []),
-        do: process_queryable(:one!, [queryable, opts])
+        do: process_queryable(:one!, &super(&1, &2), [queryable, opts])
 
       def aggregate(queryable, aggregate, field, opts \\ [])
           when aggregate in [:count, :avg, :max, :min, :sum] and is_atom(field),
-        do: process_queryable(:aggregate, [queryable, field, opts])
+        do: process_queryable(:aggregate, &super(&1, &2, &3, &4), [queryable, field, opts])
 
       def insert_all(schema_or_source, entries, opts \\ []),
-        do: process_schema(:insert_all, [schema_or_source, entries, opts])
+        do: process_schema(:insert_all, &super(&1, &2, &3), [schema_or_source, entries, opts])
 
       def update_all(queryable, updates, opts \\ []),
-        do: process_queryable(:update_all, [queryable, updates, opts])
+        do: process_queryable(:update_all, &super(&1, &2, &3), [queryable, updates, opts])
 
       def delete_all(queryable, opts \\ []),
-        do: process_queryable(:delete_all, [queryable, opts])
+        do: process_queryable(:delete_all, &super(&1, &2), [queryable, opts])
 
       def insert(struct, opts \\ []),
-        do: process_schema(:insert, [struct, opts])
+        do: process_schema(:insert, &super(&1, &2), [struct, opts])
 
       def update(struct, opts \\ []),
-        do: process_schema(:update, [struct, opts])
+        do: process_schema(:update, &super(&1, &2), [struct, opts])
 
       def insert_or_update(changeset, opts \\ []),
-        do: process_schema(:insert_or_update, [changeset, opts])
+        do: process_schema(:insert_or_update, &super(&1, &2), [changeset, opts])
 
       def delete(struct, opts \\ []),
-        do: process_schema(:delete, [struct, opts])
+        do: process_schema(:delete, &super(&1, &2), [struct, opts])
 
       def insert!(struct, opts \\ []),
-        do: process_schema(:insert!, [struct, opts])
+        do: process_schema(:insert!, &super(&1, &2), [struct, opts])
 
       def update!(struct, opts \\ []),
-        do: process_schema(:update!, [struct, opts])
+        do: process_schema(:update!, &super(&1, &2), [struct, opts])
 
       def insert_or_update!(changeset, opts \\ []),
-        do: process_schema(:insert_or_update!, [changeset, opts])
+        do: process_schema(:insert_or_update!, &super(&1, &2), [changeset, opts])
 
       def delete!(struct, opts \\ []),
-        do: process_schema(:delete!, [struct, opts])
+        do: process_schema(:delete!, &super(&1, &2), [struct, opts])
 
       def load(schema_or_types, data),
-        do: process_schema(:load, [schema_or_types, data])
+        do: process_schema(:load, &super(&1, &2), [schema_or_types, data])
 
       def preload(struct_or_structs_or_nil, preloads, opts \\ [])
       def preload(nil, preloads, opts), do: super(nil, preloads, opts)
       def preload(struct, preloads, opts) when is_map(struct),
-        do: process_schema(:preload, [struct, preloads, opts])
+        do: process_schema(:preload, &super(&1, &2, &3), [struct, preloads, opts])
 
       def preload(structs, preloads, opts) when is_list(structs) do
         if sample = Enum.find(structs, & &1) do
